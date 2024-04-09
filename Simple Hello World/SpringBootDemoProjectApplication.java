a51eaec7990087b6e4598086cfe6d054bc6ab9f8
Suchitra V <88223440+Suchitra-V31@users.noreply.github.com>
2024-04-09 15:33:04 +0530

    Update SpringBootDemoProjectApplication.java

--- Simple Hello World/SpringBootDemoProjectApplication.java
+++ Simple Hello World/SpringBootDemoProjectApplication.java
@@ -2,6 +2,8 @@ package com.example.SpringBootDemoProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
@@ -10,6 +12,8 @@ public class SpringBootDemoProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoProjectApplication.class, args);
 	}
-	
-
+	@GetMapping("/greet")
+	public String greetHello(){
+		return "Hello World!!!";
+	}
 }
