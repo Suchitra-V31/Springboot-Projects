fc3e4d239e48c11cb18625be70fbc97670482806
Suchitra V <88223440+Suchitra-V31@users.noreply.github.com>
2024-04-09 15:27:04 +0530

    Add files via upload

---
+++ Simple Hello World/SpringBootDemoProjectApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringBootDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoProjectApplication.class, args);
+	}
+	
+
+}
